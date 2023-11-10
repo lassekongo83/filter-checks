ef5a6eb22e310bf3c67af068c4d0c2b4494de902
Mattias <lassekongo83@users.noreply.github.com>
2023-11-10 20:16:09 +0100

    Create extract_domains.py

---
+++ tools/extract_domains.py
@@ -0,0 +1,26 @@
+import os
+import re
+
+INPUT_FILE = 'lists/main.txt'
+OUTPUT = 'logs/domains.txt'
+
+with open(INPUT_FILE, 'r') as f:
+  lines = f.readlines()
+
+cosmetic_filters = [re.sub(r'#.*', '', line) for line in lines if '##' in line]
+domain_specific = [re.sub(r'.*domain=', '', line) for line in lines if 'domain=' in line]
+domain_from_filters = [match[0][1] for line in lines if ('||' in line or '@@||' in line) for match in [re.findall(r'^(@@\|\||\|\|)(.*?)(\^|/)', line)] if match]
+
+lines = cosmetic_filters + domain_specific + domain_from_filters
+lines = [line.strip() for line in lines if line.strip()]
+lines = [line for line in lines if not any(c in line for c in '*')]
+lines = [line.replace('~', '') for line in lines]
+lines = [line.split('$')[0] for line in lines] # Remove everything after $
+lines = [line.split('|')[0] for line in lines] # Remove everything after |
+lines = [line.split(',')[0] for line in lines] # Remove everything after ,
+lines = [line for line in lines if line.endswith(tuple('abcdefghijklmnopqrstuvwxyz'))] # Remove lines that end with any character other than a-z
+lines = sorted(set(lines), key=lines.index)
+lines = sorted(lines)
+
+with open(OUTPUT, 'w') as f:
+  f.writelines(line + '\n' for line in lines)
