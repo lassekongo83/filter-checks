87d3a3213b3d634ba5abaef4e343c7e6da7056cf
Mattias <lassekongo83@users.noreply.github.com>
2023-11-11 12:23:43 +0100

    Update check_domain_status.py

--- tools/check_domain_status.py
+++ tools/check_domain_status.py
@@ -1,82 +1,25 @@
-import socket
-import logging
-import whois
-import http.client
+import subprocess
+import os
 from concurrent.futures import ThreadPoolExecutor
 
-# Create separate logger objects for each log file
-dns_logger = logging.getLogger('dns')
-whois_logger = logging.getLogger('whois')
-http_logger = logging.getLogger('http')
-
-# Configure each logger object to write to a different log file
-dns_handler = logging.FileHandler('logs/dns/inactive.log')
-whois_handler = logging.FileHandler('logs/whois/inactive.log')
-http_handler = logging.FileHandler('logs/httpstatus/logfile.log')
-
-# Set the level of the handlers to INFO
-dns_handler.setLevel(logging.INFO)
-whois_handler.setLevel(logging.INFO)
-http_handler.setLevel(logging.INFO)
-
-dns_logger.addHandler(dns_handler)
-whois_logger.addHandler(whois_handler)
-http_logger.addHandler(http_handler)
-
-# Set the level of the loggers to INFO
-dns_logger.setLevel(logging.INFO)
-whois_logger.setLevel(logging.INFO)
-http_logger.setLevel(logging.INFO)
-
-# Function to perform DNS lookup
-def is_domain_active(domain):
- try:
-  socket.gethostbyname(domain)
-  return True
- except socket.gaierror:
-  dns_logger.info(f"{domain} is inactive")
-  return False
-
-# Function to perform WHOIS lookup
-def get_whois_info(domain):
- try:
-  return whois.whois(domain)
- except Exception as e:
-  whois_logger.info(f"Error getting WHOIS info for {domain}: {e}")
-  return None
-
-# Function to perform HTTP status code check
-def get_http_status(domain):
- try:
-  conn = http.client.HTTPConnection(domain)
-  conn.request("GET", "/")
-  r1 = conn.getresponse()
-  return r1.status, r1.reason
- except Exception as e:
-  http_logger.info(f"Error getting HTTP status for {domain}: {e}")
-  return None, None
-
-# Read the file containing the domains
+# Read the domains from the text file
 with open('logs/domains.txt', 'r') as file:
- domains = [line.strip() for line in file]
+ domains = file.read().splitlines()
 
-# Perform DNS lookups in parallel
+# Open the log file
+log_file = open('logs/inactive_domains.log', 'w')
+
+# Function to check the DNS records of a domain
+def check_domain(domain):
+ try:
+     subprocess.check_output(['nslookup', '-q=mx', domain])
+ except subprocess.CalledProcessError:
+     log_file.write(f'{domain}\n')
+
+# Create a ThreadPoolExecutor
 with ThreadPoolExecutor(max_workers=100) as executor:
- dns_results = executor.map(is_domain_active, domains)
+ # Use the executor to map the function to the domains
+ executor.map(check_domain, domains)
 
-# Perform WHOIS lookups in parallel
-with ThreadPoolExecutor(max_workers=100) as executor:
- whois_results = executor.map(get_whois_info, domains)
-
-# Perform HTTP status code checks in parallel
-with ThreadPoolExecutor(max_workers=100) as executor:
- http_results = executor.map(get_http_status, domains)
-
-# Process the results
-for domain, dns_result, whois_result, http_result in zip(domains, dns_results, whois_results, http_results):
- if not dns_result:
-   dns_logger.info(f"{domain} is inactive")
- if whois_result is None:
-   whois_logger.info(f"{domain} is inactive")
- if http_result is None:
-   http_logger.info(f"{domain} is inactive")
+# Close the log file
+log_file.close()
