4f9fed01b21742ee026f173fadb18e8c6dc7c741
Mattias <lassekongo83@users.noreply.github.com>
2023-11-15 12:17:20 +0100

    Update check-redirects.py

--- tools/check-redirects.py
+++ tools/check-redirects.py
@@ -1,6 +1,7 @@
 import requests
 import logging
 from urllib.parse import urlparse
+from concurrent.futures import ThreadPoolExecutor
 
 # Set up the logger
 logging.basicConfig(filename='logs/redirects.log', level=logging.INFO, filemode='w')
@@ -9,16 +10,19 @@ logging.basicConfig(filename='logs/redirects.log', level=logging.INFO, filemode=
 with open('logs/domains.txt', 'r') as file:
    domains = file.readlines()
 
-# Check for redirects
-for domain in domains:
- # Add http:// to the domain if it doesn't already have a protocol
+# Function to check for redirects
+def check_redirect(domain):
  url = domain.strip()
  if not url.startswith('http://') and not url.startswith('https://'):
-   url = 'http://' + url
+  url = 'http://' + url
  response = requests.get(url)
  if response.url != url:
-   # Parse the URLs and compare the domain names
-   initial_domain = urlparse(url).netloc
-   final_domain = urlparse(response.url).netloc
-   if initial_domain != final_domain:
-       logging.info(f"{url} redirects to {response.url}")
+  # Parse the URLs and compare the domain names
+  initial_domain = urlparse(url).netloc
+  final_domain = urlparse(response.url).netloc
+  if initial_domain != final_domain:
+      logging.info(f"{url} redirects to {response.url}")
+
+# Use ThreadPoolExecutor to run the function in parallel
+with ThreadPoolExecutor(max_workers=100) as executor:
+ executor.map(check_redirect, domains)
