34ed421ef9afc057965523ec9bec841c16babb38
Mattias <lassekongo83@users.noreply.github.com>
2023-12-23 14:39:23 +0100

    Update check-css.py

--- tools/check-css/check-css.py
+++ tools/check-css/check-css.py
@@ -1,22 +1,46 @@
-# Requires: pip install requests beautifulsoup4
+# Requires: beautifulsoup4
 
 import sys
 import requests
+import getopt
 from bs4 import BeautifulSoup
 
 def check_css_selectors(url, selectors):
   response = requests.get(url)
   soup = BeautifulSoup(response.text, 'html.parser')
+  not_found_selectors = []
 
   for selector in selectors:
-    if soup.select(selector):
-      print(f'Selector "{selector}" found in the website.')
-    else:
-      print(f'Selector "{selector}" not found in the website.')
+    if selector and not soup.select(selector):
+      not_found_selectors.append(selector)
+
+  if not_found_selectors:
+    print('Selectors NOT found in the website:')
+    for selector in not_found_selectors:
+      print(f'{selector}')
+
+def main():
+  try:
+    opts, _ = getopt.getopt(sys.argv[1:], '', ['site=', 'css='])
+  except getopt.GetoptError:
+    print('Invalid argument')
+    sys.exit(2)
+
+  site = None
+  css = None
+  for opt, arg in opts:
+    if opt == '--site':
+      site = arg
+    elif opt == '--css':
+      css = arg.split(',')
+
+  if site and css:
+    check_css_selectors(site, css)
+  else:
+    print('Missing arguments')
+    sys.exit(2)
 
 if __name__ == "__main__":
-  url = sys.argv[1]
-  selectors = sys.argv[2].split(',')
-  check_css_selectors(url, selectors)
+  main()
 
-# Run with: python css-check.py site=https://example.com css=.class1,#id1,div[class=class2],[class*=class3]
+# Run with: python css-check.py --site https://example.com --css .class1,#id1,div[class="class2"],[class*="class3"]
