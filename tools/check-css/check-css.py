4ca8817bcd57630c7f6ad293a47404574cc34d2e
Mattias <lassekongo83@users.noreply.github.com>
2023-12-24 06:10:17 +0100

    Update check-css.py

--- tools/check-css/check-css.py
+++ tools/check-css/check-css.py
@@ -1,18 +1,42 @@
-# Requires: beautifulsoup4
-
 import sys
 import requests
 import getopt
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 
 def check_css_selectors(url, selectors):
   response = requests.get(url)
   soup = BeautifulSoup(response.text, 'html.parser')
   not_found_selectors = []
 
+  selectors = selectors.split(',') # Split the css string into a list of selectors
+
   for selector in selectors:
-    if selector and not soup.select(selector):
+    if selector.startswith('['): # Check if the selector is an attribute selector
+      tag, attr = selector.split('[')[0], selector.split('[')[1].replace(']', '')
+      attr_parts = attr.split('*')
+      if len(attr_parts) == 2: # Handle * selector
+        elements = soup.find_all(tag, attrs={attr_parts[0]: lambda x: x and attr_parts[1] in x})
+      elif len(attr_parts) == 3: # Handle ^ and $ selectors
+        start, end = attr_parts[0], attr_parts[2]
+        elements = soup.find_all(tag, attrs={attr_parts[0]: lambda x: x and (x.startswith(start) if end == '$' else x.endswith(end))})
+    else:
+      elements = soup.find_all(selector)
+      
+    if not elements:
       not_found_selectors.append(selector)
+
+  # Check CSS files
+  css_links = soup.find_all('link', {'rel': 'stylesheet'})
+  for link in css_links:
+    css_url = link.get('href')
+    if not css_url.startswith('https'):
+      css_url = urljoin(url, css_url)
+      css_response = requests.get(css_url)
+      css_content = css_response.text
+    for selector in selectors:
+      if selector in css_content:
+        not_found_selectors.remove(selector)
 
   if not_found_selectors:
     print('Selectors NOT found in the website:')
@@ -32,7 +56,7 @@ def main():
     if opt == '--site':
       site = arg
     elif opt == '--css':
-      css = arg.split(',')
+      css = arg # Pass css as a single string
 
   if site and css:
     check_css_selectors(site, css)
@@ -42,5 +66,3 @@ def main():
 
 if __name__ == "__main__":
   main()
-
-# Run with: python css-check.py --site https://example.com --css .class1,#id1,div[class="class2"],[class*="class3"]
